a3d7ef3108c7b48dd6d358fdb549951573247868
Alejandro <aariasmartijena@gmail.com>
2021-11-10 11:16:39 -0400

    delete gh-pages branch

---
+++ webpack.config 2.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Sushi Restaurant Page',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+                generator: {
+                    filename: 'stylesheets/[name][ext][query]'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext][query]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
