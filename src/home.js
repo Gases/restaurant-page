e2e108c50b5b138c8304bd832a2de1db85439e84
Alejandro <aariasmartijena@gmail.com>
2021-11-10 11:05:58 -0400

    added content to the home page

--- src/home.js
+++ src/home.js
@@ -10,7 +10,17 @@ const homePage = () => {
     body.className = '';
     body.classList.add('home-page');
 
-
+    div.innerHTML = `
+    <h1 class="title"> This is a sample sushi restaurant webpage!</h1>
+    <div>This page was made as an exercise for The Odin Project. It serves no other purpose than to teach me how to use WebPack. And it worked! I've learned a lot about WebPack in the past week. I ran into some problems but they were solved using StackOverflow and the WebPack documentation.</div>
+    <br/>
+    <div>Here are some drawings of sushi:</div>
+    <div class="sushi-images">
+        <div class="sushi-cartoon image-1"> </div>
+        <div class="sushi-cartoon image-2"> </div>
+        <div class="sushi-cartoon image-3"> </div>
+    </div>
+    `;
 
     content.appendChild(div);
 }
